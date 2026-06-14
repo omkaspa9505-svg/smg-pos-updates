9f371e3dfb1fccae6d004327149821dee10c3491
SMG Pos <omkas@example.com>
2026-06-14 16:21:27 +0530

    Fix build bracket syntax error

--- electron/main.ts
+++ electron/main.ts
@@ -300,6 +300,8 @@ app.whenReady().then(async () => {
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
+})
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
